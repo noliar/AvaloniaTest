7d9b5e41d9f99f973acd70182fa1fc4e5e840566
杜晓宇 <noliar@qq.com>
2024-05-07 07:58:57 +0800

    init

---
+++ MyAppBeta1/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace MyAppBeta1;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
---
+++ MyAppBeta2/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace MyAppBeta2;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
